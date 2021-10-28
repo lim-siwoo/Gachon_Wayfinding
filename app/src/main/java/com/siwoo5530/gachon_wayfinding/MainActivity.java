54ff5a8fead3b77912896c77200cc22eb3dbc684
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2021-10-28 18:43:27 +0900

    make project

---
+++ app/src/main/java/com/siwoo5530/gachon_wayfinding/MainActivity.java
@@ -0,0 +1,14 @@
+package com.siwoo5530.gachon_wayfinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
