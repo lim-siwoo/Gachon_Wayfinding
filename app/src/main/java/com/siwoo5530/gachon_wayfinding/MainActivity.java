018929f7c15219f500387d819927ba5b8a9de54a
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2021-11-04 16:02:51 +0900

    Onclick Test

--- app/src/main/java/com/siwoo5530/gachon_wayfinding/MainActivity.java
+++ app/src/main/java/com/siwoo5530/gachon_wayfinding/MainActivity.java
@@ -1,14 +1,30 @@
 package com.siwoo5530.gachon_wayfinding;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText et_id;
+    Button btn_test;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {//앱이 처음시작할때 실행되는 생명주기
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        et_id = findViewById(R.id.et_id);
+        btn_test = findViewById(R.id.btn_test);
+
+        btn_test.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                et_id.setText("Helloworld");
+            }
+        });
     }
 }
