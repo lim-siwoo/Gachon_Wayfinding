047f13c6f64ae7d22e47cc8fb20da58aaff638b2
임시우 (Siwoo Lim) <siwoo5530@gmail.com>
2021-11-10 16:17:44 +0900

    add spinner

--- app/src/main/java/com/siwoo5530/gachon_wayfinding/NaviActivity.java
+++ app/src/main/java/com/siwoo5530/gachon_wayfinding/NaviActivity.java
@@ -11,21 +11,12 @@ import android.widget.Toast;
 public class NaviActivity extends AppCompatActivity {
 
 
-    ImageView test;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navi);
 
-        test = (ImageView)findViewById(R.id.test);
         Intent intent = getIntent();
 
-
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"HelloWorld",Toast.LENGTH_SHORT).show();
-            }
-        });
     }
 }
